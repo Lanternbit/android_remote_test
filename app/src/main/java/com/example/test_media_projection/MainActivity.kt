799a58ce7cf88ade9f753e076c1f8bbd33166a09
ghseo <seogyeongho@seogyeonghoui-iMac.local>
2025-05-09 15:57:50 +0900

    style: change button position in MainActivity

--- app/src/main/java/com/example/test_media_projection/MainActivity.kt
+++ app/src/main/java/com/example/test_media_projection/MainActivity.kt
@@ -3,20 +3,35 @@ package com.example.test_media_projection
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.test_media_projection.ui.CaptureScreen
 import com.example.test_media_projection.viewmodel.SharedViewModel
 import com.example.test_media_projection.ui.DisplayScreen
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val sharedViewModel: SharedViewModel = viewModel()
-            if (isInMultiWindowMode) {
-                DisplayScreen(sharedViewModel)
-            } else {
-                CaptureScreen(sharedViewModel)
+            Surface (
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    val sharedViewModel: SharedViewModel = viewModel()
+                    if (isInMultiWindowMode) {
+                        DisplayScreen(sharedViewModel)
+                    } else {
+                        CaptureScreen(sharedViewModel)
+                    }
+                }
             }
         }
     }
